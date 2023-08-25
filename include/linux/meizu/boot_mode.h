cdc57717f3582b19c65be10c858af28006e02542
transaero21 <transaero21@elseboot.ru>
2023-08-25 19:05:27 +0300

    drivers: platform: meizu: Add boot mode detection

---
+++ include/linux/meizu/boot_mode.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 transaero21 <transaero21@elseboot.ru>
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef _MEIZU_BOOT_MODE_H_
+#define _MEIZU_BOOT_MODE_H_
+
+#ifdef CONFIG_MEIZU_QCOM
+
+int get_boot_mode(void);
+
+#endif /* CONFIG_MEIZU_QCOM */
+
+#endif /* _MEIZU_BOOT_MODE_H_ */
